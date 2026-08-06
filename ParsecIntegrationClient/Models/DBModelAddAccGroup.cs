32afaa4433da6be920aaccee101bbd6c985bf2fa
Андрей <andrey.buharov@gmail.com>
2026-08-06 13:26:15 +0300

    забыл добавить модель

---
+++ ParsecIntegrationClient/Models/DBModelAddAccGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsecIntegrationClient.Models
+{
+    internal class DbModelAddAccessCategory
+    {
+
+        //Кому добавлять
+        public string GUID_PEP { get; set; }
+        
+        //что добавлять
+        public string GUID_ACCGROUP { get; set; }
+        
+    }
+}
